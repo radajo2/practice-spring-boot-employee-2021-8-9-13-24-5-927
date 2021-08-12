59a4d51c99f5a4fc8f9cc163bd92f172136772d3
RADAJO2 <radajo2@oocl.com>
2021-08-13 07:13:30 +0800

    [Employee][Joanna] refactor: should_return_all_employee_when_call_get_employee_api - change expected valu and added testEmployees

--- src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
@@ -2,12 +2,16 @@ package com.thoughtworks.springbootemployee.integration;
 
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -21,19 +25,28 @@ public class EmployeeIntegrationTest {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    private List<Employee> testEmployees;
+
+    @BeforeEach
+    public void setup(){
+        testEmployees = Arrays.asList
+                (new Employee(1, "Joanna", 20, "female", 100),
+                (new Employee(2, "Suho", 30, "male", 900000)),
+                (new Employee(4, "Kyungsoo", 19, "male", 1000000)),
+                (new Employee(5, "Chanyeol", 26, "female", 1000000)),
+                (new Employee(8, "Kai", 18, "female", 1000000)),
+                (new Employee(65, "Xiumin", 25, "male", 150000)));
+    }
+
     @Test
     void should_return_all_employee_when_call_get_employee_api() throws Exception {
-        //given
-        final Employee employee = new Employee (1, "Joanna", 20, "female", 100);
-        employeeRepository.save(employee);
-        //when
         //then
         mockMvc.perform(MockMvcRequestBuilders.get("/employees"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].id").isNumber())
-                .andExpect(jsonPath("$[0].name").value("Joanna"))
-                .andExpect(jsonPath("$[0].age").value(20))
-                .andExpect(jsonPath("$[0].gender").value("female"))
-                .andExpect(jsonPath("$[0].salary").value(100));
+                .andExpect(jsonPath("$[2].id").isNumber())
+                .andExpect(jsonPath("$[2].name").value("Kyungsoo"))
+                .andExpect(jsonPath("$[2].age").value(19))
+                .andExpect(jsonPath("$[2].gender").value("male"))
+                .andExpect(jsonPath("$[2].salary").value(1000000));
     }
 }
