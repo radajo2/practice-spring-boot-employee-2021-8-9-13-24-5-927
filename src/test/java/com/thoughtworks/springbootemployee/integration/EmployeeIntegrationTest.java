01dba8edb319cf1bfe33785df5ffeef0083c62e5
RADAJO2 <radajo2@oocl.com>
2021-08-12 17:52:30 +0800

    [PairProgramming][Joanna] test: should_return_all_employee_when_call_get_employee_api

---
+++ src/test/java/com/thoughtworks/springbootemployee/integration/EmployeeIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.thoughtworks.springbootemployee.integration;
+
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    void should_return_all_employee_when_call_get_employee_api() throws Exception {
+        //given
+        final Employee employee = new Employee (3, "Joanna", 20, "male", 100);
+        employeeRepository.save(employee);
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.get("/employees"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").isNumber())
+                .andExpect(jsonPath("$[0].name").value("David"))
+                .andExpect(jsonPath("$[0].age").value(20))
+                .andExpect(jsonPath("$[0].gender").value("male"))
+                .andExpect(jsonPath("$[0].salary").value(100));
+    }
+}
