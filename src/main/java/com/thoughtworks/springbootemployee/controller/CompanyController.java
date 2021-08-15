a437a96694a5bd98d4518514b3160551bdbd1ab9
RADAJO2 <radajo2@oocl.com>
2021-08-15 21:50:59 +0800

    [Employee][Joanna] refactor: added company API

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -4,10 +4,8 @@ import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,6 +23,32 @@ public class CompanyController {
     @GetMapping(path = "/{companyId}/employees")
     public List<Employee> getAllEmployeesByCompanyId(@PathVariable Integer companyId){
         return companyService.getAllEmployeesByCompanyId(companyId);
+    }
+
+    @GetMapping(path = "/{companyId}")
+    public Company findById(@PathVariable Integer companyId){
+        return companyService.findById(companyId);
+    }
+
+    @GetMapping(params = {"pageIndex", "pageSize"})
+    public List<Company> getCompaniesByPage(@RequestParam Integer pageIndex, @RequestParam Integer pageSize) {
+        return companyService.getCompaniesByPage(pageIndex, pageSize);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Company addCompany(@RequestBody Company companyInfo){
+        return companyService.addCompany(companyInfo);
+    }
+
+    @PutMapping(path = "/{companyId}")
+    public Company updateCompany(@PathVariable Integer companyId, @RequestBody Company companyInfo){
+        return companyService.updateCompany(companyId, companyInfo);
+    }
+
+    @DeleteMapping(path = "/{companyId}")
+    public Company deleteCompany(@PathVariable Integer companyId){
+        return companyService.removeCompany(companyId);
     }
 
 }
--- src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/CompanyService.java
@@ -3,10 +3,13 @@ package com.thoughtworks.springbootemployee.service;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CompanyService {
@@ -21,8 +24,44 @@ public class CompanyService {
         return companyRepository.findAll();
     }
 
+    public Company findById(Integer companyId) {
+        return companyRepository.findById(companyId).orElse(null);
+    }
+
     public List<Employee> getAllEmployeesByCompanyId(Integer companyId) {
         Company company = companyRepository.findById(companyId).orElse(null);
         return company.getEmployees();
+    }
+
+    public List<Company> getCompaniesByPage(Integer pageIndex, Integer pageSize) {
+        return companyRepository.findAll(PageRequest.of(pageIndex-1,pageSize)).getContent();
+    }
+
+    public Company addCompany(Company companyInfo) {
+        return companyRepository.save(companyInfo);
+    }
+
+    public Company updateCompany(Integer companyId, Company companyInfo) {
+        Company updateCompany = companyRepository
+                .findById(companyId)
+                .map(company -> updateCompanyInfo(company, companyInfo))
+                .get();
+        return companyRepository.save(updateCompany);
+    }
+
+    private Company updateCompanyInfo(Company company, Company companyInfo) {
+        if (companyInfo.getCompany_name() != null){
+            company.setCompany_name(companyInfo.getCompany_name());
+        }
+        if (!companyInfo.getEmployees().isEmpty() && companyInfo.getEmployees() != null){
+            company.setEmployees(companyInfo.getEmployees());
+        }
+        return company;
+    }
+
+    public Company removeCompany(Integer companyId) {
+        Optional<Company> removeCompany = companyRepository.findById(companyId);
+        companyRepository.deleteById(companyId);
+        return removeCompany.orElse(null);
     }
 }
