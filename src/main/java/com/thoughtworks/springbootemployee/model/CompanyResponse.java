0b10f13d992cd88009189ea3658996d51d109fc4
RADAJO2 <radajo2@oocl.com>
2021-08-16 00:02:50 +0800

    [Employee][Joanna] feat: added CompanyResponse and CompanyMapper

---
+++ src/main/java/com/thoughtworks/springbootemployee/model/CompanyResponse.java
@@ -0,0 +1,35 @@
+package com.thoughtworks.springbootemployee.model;
+
+import javax.persistence.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class CompanyRequest {
+    private String company_name;
+    private List<Employee> employees = new LinkedList<>();
+
+    public CompanyRequest() {}
+
+    public CompanyRequest(String name, List<Employee> employees) {
+        this.company_name = name;
+        this.employees = employees;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public String getCompany_name() {
+        return company_name;
+    }
+
+    public void setCompany_name(String company_name) {
+        this.company_name = company_name;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
