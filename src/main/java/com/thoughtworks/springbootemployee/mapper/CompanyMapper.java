c6c29ba712254153a529fbb1616fab5b500ca0b9
RADAJO2 <radajo2@oocl.com>
2021-08-15 18:31:58 +0800

    [Employee][Joanna] feat: added CompanyMapper and CompanyResponse

--- src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
+++ src/main/java/com/thoughtworks/springbootemployee/mapper/CompanyMapper.java
@@ -1,2 +1,17 @@
-package com.thoughtworks.springbootemployee.mapper;public class CompanyMapper {
+package com.thoughtworks.springbootemployee.mapper;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.CompanyRequest;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CompanyMapper {
+//    public CompanyResponse toResponse (CompanyRequest companyRequest){
+//        CompanyResponse companyResponse = new company();
+//        BeanUtils.copyProperties(companyRequest, company);
+//
+//        return company;
+//    }
+
 }
--- src/main/java/com/thoughtworks/springbootemployee/model/CompanyRequest.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/CompanyRequest.java
@@ -1,27 +1,23 @@
 package com.thoughtworks.springbootemployee.model;
 
-import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
-
 import javax.persistence.*;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
 @Entity
-public class Company {
+public class CompanyRequest {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
     private String company_name;
 
     @OneToMany (cascade = CascadeType.ALL, mappedBy = "companyId")
     private List<Employee> employees = new LinkedList<>();
 
-    public Company() {}
+    public CompanyRequest() {}
 
-    public Company(int id, String name, List<Employee> employees) {
-        this.id = id;
+    public CompanyRequest(String name, List<Employee> employees) {
         this.company_name = name;
         this.employees = employees;
     }
@@ -30,16 +26,8 @@ public class Company {
         return employees;
     }
 
-    public Integer getId() {
-        return id;
-    }
-
     public String getCompany_name() {
         return company_name;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public void setCompany_name(String company_name) {
@@ -48,18 +36,5 @@ public class Company {
 
     public void setEmployees(List<Employee> employees) {
         this.employees = employees;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Company company = (Company) o;
-        return id == company.id && Objects.equals(company_name, company.company_name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, company_name);
     }
 }
